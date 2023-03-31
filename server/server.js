61a5ae1d292dfb9b4ebc791bee5dbb8b438ac014
Cheung Yee Cheng <johnny.cheng.sf@gmail.com>
2023-03-31 15:06:52 -0700

    v13

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
