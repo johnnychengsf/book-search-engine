1c7434ee7d7c2063ef13b1b1195afc155409d6a3
Cheung Yee Cheng <johnny.cheng.sf@gmail.com>
2023-03-31 16:12:05 -0700

    v16

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://johnnychengsf:bnjAj5ctIIa35u5D@atlascluster.o1rfkfl.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
