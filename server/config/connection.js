81d065d10cd25e8315d71c5b71cb04b11a7d36e3
Cheung Yee Cheng <johnny.cheng.sf@gmail.com>
2023-03-31 17:55:59 -0700

    v18

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://johnnychengsf:bnjAj5ctIIa35u5D@atlascluster.o1rfkfl.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+/*
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+*/
+console.log("connect to:",process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
